779b45b1c5c5ba4f4f4530a8c769fdca9902a152
Shah-Jr <itsmuhammedshah@gmail.com>
2023-11-14 20:51:51 -0500

    First Commit

---
+++ Test1.java
@@ -0,0 +1,10 @@
+public class Test1 {
+    public static void main(String[] args) {
+
+        System.out.println("Name: Muhammed ");
+        System.out.println("Age: 30+");
+        System.out.println("Address:65 Carrera Blvd");
+        System.out.println("Profession: QA Analyst");
+
+    }
+}
