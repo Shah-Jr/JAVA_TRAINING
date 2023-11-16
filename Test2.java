2c4174c4ab009525610b7d3967be3b0d15c5191d
Shah-Jr <itsmuhammedshah@gmail.com>
2023-11-15 21:28:20 -0500

    Test2

---
+++ Test2.java
@@ -0,0 +1,40 @@
+class Test2 {
+
+    public static void main(String[] args) {
+ 
+        String myName = "Muhammed";
+        int age = 30;
+        String address = "65 Carrera Bld";
+        String profession = "QA Analyst";
+        String companyName = "NA";
+
+        System.out.println(myName);
+        System.out.println(age);
+        method2();
+        System.out.println(address);
+        method3();
+        System.out.println(profession);
+        System.out.println(companyName);
+    }
+
+        public static void method2(){
+            String streetName = "Address";
+            System.out.println(streetName);
+
+        }
+public static void method3(){
+    String profession = "Profession";
+    System.out.println(profession);
+}
+
+
+
+
+
+
+ 
+
+
+    }
+
+
