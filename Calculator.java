2b69ef63e3b55e11331f5f5f67ba6e0276263441
Shah-Jr <itsmuhammedshah@gmail.com>
2023-11-15 21:25:58 -0500

    Calculator

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int value1= 1;
+        int value2= 1;
+        int sum;
+        sum = value1+value2;
+        method2();
+        method3();
+        method4();
+        System.out.println(sum);
+
+    }
+    public static void method2() {
+        String addition = "Addition";
+        System.out.println(addition);
+
+    }
+    public static void method4(){
+        String result = "Result";
+        System.out.println(result);
+
+        
+    }
+    public static void method3() {
+        String value = "1+1";
+        System.out.println(value);
+        
+    }
+}
