ce1b3a154793ece7cca8c4d1532e539d55dc2944
Shah-Jr <itsmuhammedshah@gmail.com>
2023-11-22 01:09:17 -0500

    Array

---
+++ Array.java
@@ -0,0 +1,26 @@
+public class Array {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+    String[] classmateNames = {"Akshay", "Shyamili", "Sha"};
+    Integer[] roleNum = {1,2,3,4};
+    //method2();
+    System.out.println("Single Dimensional Array Example:");
+    for (int i=0; i < classmateNames.length; i++){
+        System.out.println(classmateNames [i]);
+    }
+
+    }
+}
+
+/* 
+    private static void method2() {
+
+     //   String filename = "Single Dimensional Array Example:";
+
+        System.out.println("Single Dimensional Array Example:");
+    }
+    
+}
+*/
